dfbd0e2fe8689e428dfefa2b7237157760d904eb
Alex Plante <alex.plante17@gmail.com>
2022-11-16 13:22:22 -0500

    tenant,vrf,bd,epg,app

--- cdktf/main.py
+++ cdktf/main.py
@@ -3,9 +3,18 @@ from constructs import Construct
 
 from cdktf import App, TerraformStack
 
-#from . import AciProvider
+
 from imports.aci import provider
+
 from imports.aci import tenant
+
+from imports.aci import vrf
+
+from imports.aci import bridge_domain
+from imports.aci import subnet
+
+from imports.aci import application_profile
+from imports.aci import application_epg
 
 
 class MyStack(TerraformStack):
@@ -15,16 +24,49 @@ class MyStack(TerraformStack):
         aci = provider.AciProvider(self, "aci_provider",
             url = "https://10.10.20.14",
             username = "admin",
-            password = "C1sc012345",
+            password = "C1sco12345",
             insecure = True
         )
 
         # define resources here
 
-        vanilla_tenant = tenant.Tenant(self,
+        ## Tenant 
+        terraform_cdktf_tenant = tenant.Tenant(self, "terraform_cdktf_tenant",
         name = "terraform_cdktf_tenant",
         description = "This tenant was built with CDKTF using Pyhton"
         )
+
+        ## VRF
+        terraform_cdktf_vrf = vrf.Vrf(self, "terraform_cdktf_vrf",
+        name = "terraform_cdk_VRF",
+        tenant_dn = terraform_cdktf_tenant.id)
+
+        ## BD
+
+        terraform_cdktf_bd_1 = bridge_domain.BridgeDomain(self, "terraform_cdktf_bd_1",
+        name = "terraform_cdktf_bd_1",
+        tenant_dn = terraform_cdktf_tenant.id,
+        relation_fv_rs_ctx = terraform_cdktf_vrf.id)
+
+        ## Subnet
+        terraform_cdktf_subnet_1 = subnet.Subnet(self, "terraform_cdktf_subnet_1",
+        parent_dn = terraform_cdktf_bd_1.id,
+        ip = "10.0.0.1/24",
+        scope = [ "public" ]
+        )
+
+        ## Application Profile
+
+        terraform_cdktf_app = application_profile.ApplicationProfile(self, "terraform_cdktf_app",
+        name = "terraform_cdktf_app",
+        tenant_dn = terraform_cdktf_tenant.id)
+        
+        ## EPG
+
+        terraform_cdktf_epg_1 = application_epg.ApplicationEpg(self, "terraform_cdktf_epg_1",
+        name = "terraform_cdktf_epg_1",
+        application_profile_dn= terraform_cdktf_app.id,
+        relation_fv_rs_bd = terraform_cdktf_bd_1.id)
 
 
 app = App()
--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aci" {
   # cisco-aci user name
   username = "admin"
   # cisco-aci password
-  password = "C1sc012345"
+  password = "C1sco12345"
   # cisco-aci url
   url      = "https://10.10.20.14"
   # Use insecure HTTP
@@ -20,12 +20,42 @@ provider "aci" {
 }
 
 # Ressource configuration
-resource "aci_tenant" "vanilla" {
-  name = "vanilla"
+
+# Tenant
+resource "aci_tenant" "tenantLocalName" {
+    name = "vanilla"
 }
 
+# VRF
+resource "aci_vrf" "vrfLocalName" {
+    name = "global_vrf"
+    tenant_dn = aci_tenant.tenantLocalName.id
+}
 
-resource "mso_tenant" "vanilla" {
-  name = "vanilla"
-  display_name = "vanilla"
+# Bridge Domain
+resource "aci_bridge_domain" "bdLocalName" {
+  name = "global_bd"
+  tenant_dn = aci_tenant.tenantLocalName.id
+  relation_fv_rs_ctx = aci_vrf.vrfLocalName.id
+}
+
+# Subnet
+resource "aci_subnet" "subnetLocalName" {
+    parent_dn = aci_bridge_domain.bdLocalName.id
+    ip = "10.0.0.1/24"
+    scope = [ "public" ]
+}
+
+# Application Profile
+resource "aci_application_profile" "apLocalName" {
+  tenant_dn = aci_tenant.tenantLocalName.id
+  name = "3tier_ap"
+}
+
+# Web EPG
+resource "aci_application_epg" "webEpgLocalName" {
+  name = "web_epg"
+  application_profile_dn = aci_application_profile.apLocalName.id
+  relation_fv_rs_bd = aci_bridge_domain.bdLocalName.id
+  pref_gr_memb = "include"
 }
